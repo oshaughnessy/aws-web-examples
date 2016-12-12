b6a9f71274db4145fe29040087615cf55987d9b8
O'Shaughnessy Evans <shaug@act-on.net>
2016-12-11 22:30:01 -0800

    add /var/www to the Puppet manifest; adjust the HTML page title

--- vagrant-puppet/manifests/default.pp
+++ vagrant-puppet/manifests/default.pp
@@ -1,5 +1,22 @@
+File {
+  owner   => 'root',
+  group   => 'root',
+}
+
+file { '/var/www':
+  ensure  => directory,
+  mode    => '0755',
+}
+
+file { '/var/www/vagrant':
+  ensure  => directory,
+  mode    => '0755',
+  recurse => true,
+  source  => '/vagrant/public',
+}
+
 class { 'nginx': }
 
 nginx::resource::vhost { 'localhost':
-  www_root => '/vagrant/public',
+  www_root => '/var/www/vagrant',
 }
